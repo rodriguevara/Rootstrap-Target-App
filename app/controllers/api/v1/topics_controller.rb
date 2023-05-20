fef168dab6065369b51e512e748c8f810ea3b3f2
rodrigo ladron de guevara <ro.ladrondeguevara@gmail.com>
2023-05-20 11:22:58 -0300

    small fixes

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class TopicsController < Api::V1::ApiController
       def index
-        render json: @topics = policy_scope(Topic), only: %i[id label], include: [:image]
+        @topics = policy_scope(Topic)
       end
     end
   end
