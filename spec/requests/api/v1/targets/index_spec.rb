76b13cc75b6cd4f76e01ef70280dfd6823ad3fc0
rodrigo ladron de guevara <ro.ladrondeguevara@gmail.com>
2023-05-25 11:34:37 -0300

    RSLC-3 As a user I should be able to see all my targets in the map so I can easily identify them v2

--- spec/requests/api/v1/targets/index_spec.rb
+++ spec/requests/api/v1/targets/index_spec.rb
@@ -19,8 +19,8 @@ describe 'GET api/v1/targets', type: :request do
     subject
     expect(json[:targets].first[:title]).to eq(first_target.title)
     expect(json[:targets].first[:radius]).to eq(first_target.radius)
-    expect(json[:targets].first[:lat]).to eq(first_target.lat)
-    expect(json[:targets].first[:lon]).to eq(first_target.lon)
+    expect(json[:targets].first[:lat].round(10)).to eq(first_target.lat.round(10))
+    expect(json[:targets].first[:lon].round(10)).to eq(first_target.lon.round(10))
     expect(json[:targets].first[:topic_id]).to eq(first_target.topic_id)
   end
 end
