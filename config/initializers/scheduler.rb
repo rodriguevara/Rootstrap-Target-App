009eee9a572b6dfb42ef69d701b02d02c9193074
rodrigo ladron de guevara <ro.ladrondeguevara@gmail.com>
2023-06-09 18:14:56 -0300

    RSLC-9 As a system, I should delete targets after one week

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -3,8 +3,6 @@ require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.singleton
 
 scheduler.every '7d' do
-  @expired_targets = Target.where("created_at < ?", 7.day.ago)
-  @expired_targets.each do |target|
-    target.destroy!
-  end
+  @expired_targets = Target.where('created_at < ?', 7.days.ago)
+  @expired_targets.each(&:destroy!)
 end
