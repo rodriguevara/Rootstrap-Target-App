8942b4fd62efc8e6729ac60e3ec4ed87966ea141
rodrigo ladron de guevara <ro.ladrondeguevara@gmail.com>
2023-06-09 10:43:36 -0300

    RSLC-6 As a user I should be able to see all my conversations

--- app/models/conversations_user.rb
+++ app/models/conversations_user.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: conversations_users
+#
+#  conversation_id :bigint           not null
+#  user_id         :bigint           not null
+#
+# Indexes
+#
+#  index_conversations_users_on_conversation_id_and_user_id  (conversation_id,user_id)
+#
 class ConversationsUser < ApplicationRecord
   belongs_to :conversation
   belongs_to :user
