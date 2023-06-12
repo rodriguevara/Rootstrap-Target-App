9a6eea0756e6da39c6c06bc8dda57033839bcfd1
rodrigo ladron de guevara <ro.ladrondeguevara@gmail.com>
2023-06-12 09:44:14 -0300

    RSLC-6 As a user I should be able to see all my conversations

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -11,10 +11,10 @@ class Conversation < ApplicationRecord
   has_many :conversations_users, dependent: :destroy
   has_many :users, through: :conversations_users
 
-  def self.create_chat(current_user, other_user, topic_id)
+  def self.create_chat(current_user, other_user, topicid)
     return if already_created?(current_user, other_user)
 
-    conversation = current_user.conversations.create!(topic_id: topic_id)
+    conversation = current_user.conversations.create!(topic_id: topicid)
     other_user.conversations << conversation
     conversation
   end
