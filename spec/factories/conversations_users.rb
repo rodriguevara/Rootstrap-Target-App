5a30ca52adf84983ff10e8c8d703c7b53ac8b00e
Rodrigo Ladron de Guevara <ro.ladrondeguevara@gmail.com>
2023-07-06 14:22:22 -0300

    RSLC-6 As a user I should be able to see all my conversations

--- spec/factories/conversations_users.rb
+++ spec/factories/conversations_users.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: conversations_users
+#
+#  conversation_id :bigint           not null
+#  user_id         :bigint           not null
+#
+# Indexes
+#
+#  index_conversations_users_on_conversation_id_and_user_id  (conversation_id,user_id)
+#
 FactoryBot.define do
   factory :conversations_user do
     user
