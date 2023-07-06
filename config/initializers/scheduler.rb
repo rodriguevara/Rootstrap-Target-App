ce00a0b45665f6df71c4b0010aa516d7846d0795
Rodrigo <104015392+rodriguevara@users.noreply.github.com>
2023-07-06 16:25:55 -0300

    Update config/initializers/scheduler.rb
    Co-authored-by: enzofab91 <enzofab91@gmail.com>

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -3,6 +3,6 @@ require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.singleton
 
 scheduler.every '1d' do
-  @expired_targets = Target.where('created_at < ?', 7.days.ago)
-  @expired_targets.each(&:destroy!)
+  expired_targets = Target.where('created_at < ?', 7.days.ago)
+  expired_targets.each(&:destroy!)
 end
